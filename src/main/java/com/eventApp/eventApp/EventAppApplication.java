15781528c4f014192e3d054bffa351ec6cb0e01a
Rodrigo Rafael <rodrigorafaeldamaceno@gmail.com>
2019-09-26 11:02:23 -0300

    added mysql connection

--- src/main/java/com/eventApp/eventApp/EventAppApplication.java
+++ src/main/java/com/eventApp/eventApp/EventAppApplication.java
@@ -1,12 +1,20 @@
 package com.eventApp.eventApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@EntityScan(basePackages = {"com.eventApp.models"})
+@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan({"com.eventApp.controllers"})
 public class EventAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(EventAppApplication.class, args);
 	}
 
---
+++ src/main/java/com/eventApp/models/Event.java
@@ -0,0 +1,64 @@
+package com.eventApp.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Event implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long id;
+
+	private String name;
+	private String location;
+	private String date;
+	private String time;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
